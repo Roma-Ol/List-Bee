5d99456d63eb2fc7350b17c6e2c41bc62858c5d7
Roman Oliievskyi <r.oliievskyi@internetdevels.com>
2022-07-20 00:42:59 +0300

    LB-3: Created list of all the todos

--- src/Components/card/card.js
+++ src/Components/card/card.js
@@ -1,9 +1,27 @@
 import {Button, ButtonGroup, Fab} from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
 import {useState} from "react";
+import CardContent from "./card-content";
 
 const ListCard = () => {
+    // Open & close card settings identifier.
     const [isOpened, setOppositeVal] = useState(false);
+
+    // All the todos of this card.
+    const [todo, setTodo] = useState([
+        {
+            uid: 1,
+            title: 'First title',
+            description: 'First description',
+            active: true
+        },
+        {
+            uid: 2,
+            title: 'Second title',
+            description: 'Second description',
+            active: true
+        }
+    ]);
 
     const buttons = [
         <Button key="one">Delete</Button>,
@@ -11,28 +29,35 @@ const ListCard = () => {
         <Button key="three">Add row (right)</Button>,
     ];
 
-    return(
+    return (
         <div className='list__card'>
-            <Fab variant="extended"
-                 size="small"
-                 color="primary"
-                 aria-label="add"
-                 onClick={() => {setOppositeVal(isOpened === false)}}>
-               Actions
-                <MenuIcon/>
-            </Fab>
+            <div className="list__card__actions">
+                <Fab variant="extended"
+                     size="small"
+                     color="primary"
+                     aria-label="add"
+                     onClick={() => {
+                         setOppositeVal(isOpened === false)
+                     }}>
+                    Actions
+                    <MenuIcon/>
+                </Fab>
 
-            {isOpened?
-                <div className="list__card__quick-actions">
-                    <ButtonGroup
-                        orientation="vertical"
-                        aria-label="vertical outlined button group">
-                        {buttons}
-                    </ButtonGroup>
-                </div>
-                :
-                null
-            }
+                {isOpened ?
+                    <div className="list__card__quick-actions">
+                        <ButtonGroup
+                            orientation="vertical"
+                            aria-label="vertical outlined button group">
+                            {buttons}
+                        </ButtonGroup>
+                    </div>
+                    :
+                    null}
+            </div>
+
+            <div className="list__card__content">
+                <CardContent todos={todo} />
+            </div>
         </div>
     )
 }
